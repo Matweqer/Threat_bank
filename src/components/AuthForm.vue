3104c0568d8e97361086630934b43cb3af84a14c
matweqer <matweqer@mail.ru>
2022-05-21 12:50:07 +0300

    create: auth page

---
+++ src/components/AuthForm.vue
@@ -0,0 +1,69 @@
+<template>
+  <form class="form" action="">
+    <label class="form__login"> <input type="text" placeholder="Логин или почта"> </label>
+    <label class="form__password"> <input type="password" placeholder="Пароль"> <img src="@/assets/images/auth-page/password-eye.png" alt="eye" class="form__password__eye"> </label>
+<!--   Стоит ли выносить эту ссылку в компонент строницы ??? -->
+    <a class="form__forgot-pass" href="">Забыли пароль?</a>
+
+    <button class="form__button" >Войти</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "AuthForm"
+}
+</script>
+
+<style lang="scss" scoped>
+
+@import "src/assets/scss/variables.scss";
+
+.form {
+  @include flex-column-center;
+
+    input {
+    width: 335px;
+    height: 45px;
+    border: 1px solid #646464;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 15px;
+    padding-left: 21px;
+    @include main-font-normal(22px);
+  }
+  &__login {
+     margin-top: 87px;
+   }
+  &__password {
+     position: relative;
+     margin-top: 21px;
+  &__eye {
+     position: absolute;
+     top: 13px;
+     right: 13px;
+     width: 23px;
+     height: 18px;
+     z-index: 10;
+   }
+  &__eye:hover {
+     cursor: pointer;
+   }
+  }
+  &__button {
+     width: 136px;
+     height: 45px;
+     background-color: $main-color;
+     border-radius: 10px;
+     border: none;
+     margin-top: 47px;
+     @include main-font-normal(26px);
+     color: #FFFFFF;
+   }
+  &__forgot-pass {
+     @include main-font-normal(16px);
+     margin-top: 20px;
+     color: #000000;
+     text-decoration: none;
+   }
+  }
+</style>
---
+++ src/components/AuthPage.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="auth">
+    <img class="auth__img" src="@/assets/images/auth-page/auth-bg.png" alt="background">
+    <div class="auth__container">
+      <h1 class="auth__title">Вход в систему</h1>
+      <AuthForm class="auth__form"></AuthForm>
+      <p class="auth__question">Вы еще не зарегистрированы?</p>
+      <a class="auth__registration" href="">Зарегистрироваться</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import AuthForm from "@/components/AuthForm";
+
+export default {
+  name: "AuthPage",
+  components: {
+    AuthForm
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "src/assets/scss/variables.scss";
+
+  .auth {
+    display: flex;
+    @include full-display;
+    background-color: $background-color;
+    &__img {
+      width: 60%;
+    }
+    &__container {
+      @include flex-column-center;
+      height: 100%;
+      width: 40%;
+    }
+    &__title {
+     margin-top: 130px;
+     @include main-font-normal(32px)
+   }
+    &__question {
+      @include main-font-normal(16px);
+      margin-top: 47px;
+    }
+    &__registration {
+      @include main-font-normal(16px);
+      margin-top: 20px;
+      color: #00126D;
+      text-decoration: none;
+    }
+  }
+
+</style>
